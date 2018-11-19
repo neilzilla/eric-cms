06894998cd52d215eedbb4a0c11296bc7e129713
Neil <neil@screeched.co.uk>
2018-11-19 00:55:31 +0000

    Altered root and site constants to remove trailing slashes.  Edited path to work if cms in subdir

--- derpy/env.php
+++ derpy/env.php
@@ -1,12 +1,12 @@
 <?php
     $https = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']) ? 'https://' : 'http://';
-    define('SITE_URL', $https . $_SERVER['HTTP_HOST'] . pathinfo($_SERVER['PHP_SELF'])['dirname']);
-    define('ROOT',$_SERVER['DOCUMENT_ROOT'] . pathinfo($_SERVER['PHP_SELF'])['dirname']);    
+    define('SITE_URL', rtrim($https . $_SERVER['HTTP_HOST'] . pathinfo($_SERVER['PHP_SELF'])['dirname'], '/'));
+    define('ROOT', rtrim($_SERVER['DOCUMENT_ROOT'] . pathinfo($_SERVER['PHP_SELF'])['dirname'], '/'));    
 
-    $path = $_SERVER['REQUEST_URI'];
+    $path = trim(str_replace(trim(pathinfo($_SERVER['PHP_SELF'])['dirname'], '/'), '', trim($_SERVER['REQUEST_URI'], '/')), '/');
     
-    require_once(ROOT. 'derpy/functions.php');
-    require_once(ROOT . 'derpy/lib/manifest.php');
+   require_once(ROOT. '/derpy/functions.php');
+    require_once(ROOT . '/derpy/lib/manifest.php');
 
     if(!validSlug($path)) go('home');
 
@@ -38,6 +38,6 @@
     $output = new Template($page);
     
     $output->exec();
-    
+  
 
 ?>
